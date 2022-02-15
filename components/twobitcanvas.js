af9a94507e6841690db37a7e792b81712eda3652
Jesse Doherty <jewido@gmail.com>
2022-02-15 13:52:17 -0500

    moved drawing to two-bit-drawing pointer events
    Now will draw on pointerdown/move not just click

--- components/twobitcanvas.js
+++ components/twobitcanvas.js
@@ -13,19 +13,14 @@ canvas {
 <canvas></canvas>
 `;
 
-const colours = [[0,0,0], [85, 85, 85], [171,171,171], [255,255,255]];
-function convertCoordinate(point, origin, boundingLength, pixelLength) {
-  const v = Math.floor(((point - origin) / boundingLength) * pixelLength);
-  console.log(point, origin, boundingLength, pixelLength, (point - origin), v);
-  return v;
-}
+const colours = [[0, 0, 0], [85, 85, 85], [171, 171, 171], [255, 255, 255]];
 
 
 export class TwoBitCanvas extends HTMLElement {
-    constructor () {
+    constructor() {
         super();
 
-        const shadow = this.attachShadow({mode: 'open'});
+        const shadow = this.attachShadow({ mode: 'open' });
         shadow.appendChild(template.content.cloneNode(true));
 
         this.canvas = shadow.querySelector('canvas');
@@ -35,62 +30,49 @@ export class TwoBitCanvas extends HTMLElement {
         this.imageData = imageData;
         this.ctx = ctx;
         this.updateDimensions();
-
-        this.canvas.addEventListener('click', (event) => {
-            const {x, y} = this.getMousePos(event);
-            this.setPixel(x, y, 0);
-        });
     }
 
     connectedCallback() {
-      console.log('connected', this.getAttribute('width'), this.height);
+        console.log('connected', this.getAttribute('width'), this.height);
     }
 
     static get observedAttributes() { return ['width', 'height']; }
     attributeChangedCallback(name, oldValue, newValue) {
-      if (oldValue === newValue) return;
-      console.log('change', name, oldValue, newValue, this[name]);
-      this.updateDimensions();
+        if (oldValue === newValue) return;
+        console.log('change', name, oldValue, newValue, this[name]);
+        this.updateDimensions();
     }
 
     get height() {
-      return parseInt(this.getAttribute('height')) || 0;
+        return parseInt(this.getAttribute('height')) || 0;
     }
     set height(value) {
-      console.log('height setter');
-      this.setAttribute('height', value);
+        console.log('height setter');
+        this.setAttribute('height', value);
     }
 
     get width() {
-      return parseInt(this.getAttribute('width')) || 0;
+        return parseInt(this.getAttribute('width')) || 0;
     }
     set width(value) {
-      this.setAttribute('width', value);
+        this.setAttribute('width', value);
     }
 
     updateDimensions() {
-      this.canvas.width = this.width;
-      this.canvas.height = this.height;
-      
-      this.imageData = this.ctx.createImageData(this.width, this.height);
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+
+        this.imageData = this.ctx.createImageData(this.width, this.height);
     }
 
     setPixel(x, y, v) {
-      const baseIndex = ((y * this.width) + x) * 4;
-      const [r, g, b] = colours[v];
-      this.imageData.data[baseIndex] = r;
-      this.imageData.data[baseIndex+1] = g;
-      this.imageData.data[baseIndex+2] = b;
-      this.imageData.data[baseIndex+3] = 255; // alpha
-      this.ctx.putImageData(this.imageData, 0, 0);
-    }
-
-    getMousePos(event) {
-      const rect = this.canvas.getBoundingClientRect();
-      return {
-          x: convertCoordinate(event.clientX, rect.left, rect.width, this.width),
-          y: convertCoordinate(event.clientY, rect.top, rect.height, this.height)
-      };
+        const baseIndex = ((y * this.width) + x) * 4;
+        const [r, g, b] = colours[v];
+        this.imageData.data[baseIndex] = r;
+        this.imageData.data[baseIndex + 1] = g;
+        this.imageData.data[baseIndex + 2] = b;
+        this.imageData.data[baseIndex + 3] = 255; // alpha
+        this.ctx.putImageData(this.imageData, 0, 0);
     }
 }
 
--- components/twobitdrawing.js
+++ components/twobitdrawing.js
@@ -1,4 +1,4 @@
-import {TwoBitCanvas} from './twobitcanvas.js';
+import { TwoBitCanvas } from './twobitcanvas.js';
 
 const template = document.createElement('template');
 template.innerHTML = `
@@ -15,12 +15,54 @@ two-bit-canvas {
 <two-bit-canvas width="160" height="144"></two-bit-canvas>
 `;
 
+function convertCoordinate(point, origin, boundingLength, pixelLength) {
+    const v = Math.floor(((point - origin) / boundingLength) * pixelLength);
+    console.log(point, origin, boundingLength, pixelLength, (point - origin), v);
+    return v;
+}
+
 export class TwoBitDrawing extends HTMLElement {
     constructor() {
         super();
 
-        const shadow = this.attachShadow({mode: 'open'});
+        const shadow = this.attachShadow({ mode: 'open' });
         shadow.appendChild(template.content.cloneNode(true));
+        this.twoBitCanvas = shadow.querySelector('two-bit-canvas');
+        this.isPointerDown = false;
+    }
+
+    connectedCallback() {
+        this.twoBitCanvas.onpointerdown = (event) => {
+            const { x, y } = this.getMousePos(event);
+            this.twoBitCanvas.setPixel(x, y, 0);
+            this.twoBitCanvas.onpointermove = (event) => {
+                if (event.getCoalescedEvents) {
+                    const events = event.getCoalescedEvents();
+                    if (events.length > 0) {
+                        for (let e of events) {
+                            const { x, y } = this.getMousePos(event);
+                            this.twoBitCanvas.setPixel(x, y, 0);
+                        }
+                        return;
+                    }
+                }
+                const { x, y } = this.getMousePos(event);
+                this.twoBitCanvas.setPixel(x, y, 0);
+            }
+        };
+        this.twoBitCanvas.onpointerup = (event) => {
+            const { x, y } = this.getMousePos(event);
+            this.twoBitCanvas.setPixel(x, y, 0);
+            this.twoBitCanvas.onpointermove = null;
+        };
+    }
+
+    getMousePos(event) {
+        const rect = this.twoBitCanvas.getBoundingClientRect();
+        return {
+            x: convertCoordinate(event.clientX, rect.left, rect.width, this.twoBitCanvas.width),
+            y: convertCoordinate(event.clientY, rect.top, rect.height, this.twoBitCanvas.height)
+        };
     }
 }
 customElements.define('two-bit-drawing', TwoBitDrawing);
