fb38cca905cd52f51db2e3c19646d7d034c45445
Jesse Doherty <jewido@gmail.com>
2022-02-16 13:28:36 -0500

    Use colour swatches instead of text in colour picker

--- components/twobitdrawing.js
+++ components/twobitdrawing.js
@@ -10,13 +10,31 @@ two-bit-canvas {
     cursor: crosshair;
     width: 100%;
 }
+#colour-picker div {
+    width: 1em;
+    height: 1em;
+    display: inline-block;
+}
+#swatch-0 {
+    background-color: rgb(224, 248, 208);
+}
+#swatch-1 {
+    background-color: rgb(136, 192, 112);
+}
+#swatch-2 {
+    background-color: rgb(52, 104, 86);
+}
+#swatch-3{
+    background-color: rgb(8, 24, 32);
+}
 </style>
 <two-bit-canvas width="160" height="144"></two-bit-canvas>
 <div id="colour-picker">
-<span><input type="radio" name="colour" id="c0" value="0" /><label for="c0">colour 0</label></span>
-<span><input type="radio" name="colour" id="c1" value="1" /><label for="c1">colour 1</label></span>
-<span><input type="radio" name="colour" id="c2" value="2" /><label for="c2">colour 2</label></span>
-<span><input type="radio" name="colour" id="c3" value="3" checked /><label for="c3">colour 3</label></span>
+<span><input type="radio" name="colour" id="c0" value="0" />
+<label for="c0"><div id="swatch-0"></div></label></span>
+<span><input type="radio" name="colour" id="c1" value="1" /><label for="c1"><div id="swatch-1"></div></label></span>
+<span><input type="radio" name="colour" id="c2" value="2" /><label for="c2"><div id="swatch-2"></div></label></span>
+<span><input type="radio" name="colour" id="c3" value="3" checked /><label for="c3"><div id="swatch-3"></div></label></span>
 </div>
 `;
 
