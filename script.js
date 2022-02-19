a224f8701facf3a54752c675f8016c889dd69779
Jesse Doherty <jewido@gmail.com>
2022-02-19 00:07:37 -0500

    handle edge case of multiple needDraw events before updateDrawing
    Also draw tile content when button is pressed.

--- script.js
+++ script.js
@@ -16,28 +16,32 @@ function showTiles() {
 
     const numTiles = 160*144/64;
     tiles = new Array(numTiles);
+    const tileData = drawing.getTiles();
     for (let i = 0; i < numTiles; i++) {
         const tile = document.createElement('two-bit-canvas');
         tile.width = 8;
         tile.height = 8;
+        tile.setTwoBitData(tileData.tiles[i]);
 
         tiles[i] = tile;
         tileDiv.appendChild(tile);
     }
 
     let redrawNeeded = false;
-    let tilesToUpdate = null;
+    let tilesToUpdate = [];
     drawing.addEventListener('needRedraw', (ev) => {
         redrawNeeded = true;
-        tilesToUpdate = ev.detail;
+        tilesToUpdate.push(ev.detail);
     });
     const updateDrawing = () => {
         if (redrawNeeded) {
             redrawNeeded = false;
             const t = drawing.getTiles();
-            for (const i of tilesToUpdate) {
-                if (i < 0) continue;
-                tiles[i].setTwoBitData(t.tiles[i]);
+            for (const toUpdate of tilesToUpdate) {
+                for (const i of toUpdate) {
+                    if (i < 0) continue;
+                    tiles[i].setTwoBitData(t.tiles[i]);
+                }
             }
         }
         requestAnimationFrame(updateDrawing);
