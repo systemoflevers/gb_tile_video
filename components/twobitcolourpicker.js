f46434250995678bb4689bdcd487790663dcb7cf
Jesse Doherty <jewido@gmail.com>
2022-03-14 18:01:46 -0400

    start pulling the colour picker into a component

---
+++ components/twobitcolourpicker.js
@@ -0,0 +1,66 @@
+
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+:host {
+  display: block;
+  --select-lift: 30%;
+}
+#colour-picker {
+  display: flex;
+  align-items: flex-end;
+}
+#colour-picker > div {
+  flex-grow: 1;
+}
+#colour-picker label div {
+  width: 100%;
+    aspect-ratio: 1;
+    display: inline-block;
+}
+
+#colour-picker input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    display: none;
+}
+
+#colour-picker input:checked ~ label>div {
+    margin-bottom: var(--select-lift); 
+}
+#swatch-0 {
+    background-color: rgb(224, 248, 208);
+}
+
+#swatch-1 {
+    background-color: rgb(136, 192, 112);
+}
+
+#swatch-2 {
+    background-color: rgb(52, 104, 86);
+}
+
+#swatch-3{
+    background-color: rgb(8, 24, 32);
+}
+</style>
+<div id="colour-picker">
+<div><input type="radio" name="colour" id="c0" value="0" />
+<label for="c0"><div id="swatch-0"></div></label></div>
+<div><input type="radio" name="colour" id="c1" value="1" /><label for="c1"><div id="swatch-1"></div></label></div>
+<div><input type="radio" name="colour" id="c2" value="2" /><label for="c2"><div id="swatch-2"></div></label></div>
+<div><input type="radio" name="colour" id="c3" value="3" checked /><label for="c3"><div id="swatch-3"></div></label></div>
+</div>
+`;
+
+export class TwoBitColourPicker extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.appendChild(template.content.cloneNode(true));
+
+  }
+}
+
+customElements.define('two-bit-colour-picker', TwoBitColourPicker);
